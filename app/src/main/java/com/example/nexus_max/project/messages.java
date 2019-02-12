ec1aaea31fed41d5b55ae922a2d4e0115bbc6e0f
hari <harikrishnanmidhun77@gmail.com>
2019-02-13 01:12:46 +0530

    Message almost read, Day1 end

--- app/src/main/java/com/example/nexus_max/project/messages.java
+++ app/src/main/java/com/example/nexus_max/project/messages.java
@@ -1,13 +1,52 @@
 package com.example.nexus_max.project;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class messages extends AppCompatActivity {
-
+ListView msgListView;
+    DatabaseReference dbMsg;
+    List<Message> mList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messages);
+        mList=new ArrayList<>();
+        msgListView=(ListView)findViewById(R.id.ls_msg);
+        dbMsg= FirebaseDatabase.getInstance().getReference("Messages");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        dbMsg.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mList.clear();
+                for(DataSnapshot msgSnap:dataSnapshot.getChildren()){
+                    Message msg=msgSnap.getValue(Message.class);
+                    mList.add(msg);
+                    MsgList adapter =new MsgList(messages.this,mList);
+                    msgListView.setAdapter(adapter);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        })
     }
 }
